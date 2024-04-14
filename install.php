6ffd493b43a8f3da3789f79e9550318d0ff51c79
JoeNutty <amirhusni2001@gmail.com>
2024-04-14 12:11:51 +0800

    add comment view and table

--- install.php
+++ install.php
@@ -1,12 +1,18 @@
 <?php
-// Get the PDO DSN string
 require_once 'lib/common.php';
+
+// Get the PDO DSN string
+$root = getRootPath();
+$database = getDatabasePath();
+
 $error = '';
+
 // A security measure, to avoid anyone resetting the database if it already exists
 if (is_readable($database) && filesize($database) > 0)
 {
     $error = 'Please delete the existing database manually before installing it afresh';
 }
+
 // Create an empty file for the database
 if (!$error)
 {
@@ -19,15 +25,18 @@ if (!$error)
         );
     }
 }
+
 // Grab the SQL commands we want to run on the database
 if (!$error)
 {
     $sql = file_get_contents($root . '/data/init.sql');
+
     if ($sql === false)
     {
         $error = 'Cannot find SQL file';
     }
 }
+
 // Connect to the new database and try to run the SQL commands
 if (!$error)
 {
@@ -38,17 +47,24 @@ if (!$error)
         $error = 'Could not run SQL: ' . print_r($pdo->errorInfo(), true);
     }
 }
+
 // See how many rows we created, if any
-$count = null;
-if (!$error)
+$count = array();
+
+foreach(array('post', 'comment') as $tableName)
 {
-    $sql = "SELECT COUNT(*) AS c FROM post";
-    $stmt = $pdo->query($sql);
-    if ($stmt)
+    if (!$error)
     {
-        $count = $stmt->fetchColumn();
+        $sql = "SELECT COUNT(*) AS c FROM " . $tableName;
+        $stmt = $pdo->query($sql);
+        if ($stmt)
+        {
+            // We store each count in an associative array
+            $count[$tableName] = $stmt->fetchColumn();
+        }
     }
 }
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -77,9 +93,16 @@ if (!$error)
         <?php else: ?>
             <div class="success box">
                 The database and demo data was created OK.
-                <?php if ($count): ?>
-                    <?php echo $count ?> new rows were created.
-                <?php endif ?>
+
+                <?php foreach (array('post', 'comment') as $tableName): ?>
+                    <?php if (isset($count[$tableName])): ?>
+                        <?php // Prints the count ?>
+                        <?php echo $count[$tableName] ?> new
+                        <?php // Prints the name of the thing ?>
+                        <?php echo $tableName ?>s
+                        were created.
+                    <?php endif ?>
+                <?php endforeach ?>
             </div>
         <?php endif ?>
     </body>
